ca3ee25dfd4937fbec35464db3850fb4f47dac74
mahmudul607 <111509406+mahmudul607@users.noreply.github.com>
2023-01-07 00:20:03 +0600

    start react mission

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+var style ={
+  color: 'green',
+  fontSize: '50px',
+  fontWeight: 700,
+}
 function App() {
+  var name ="Sadia";
+  var bonous =4000;
+  var  person1 ={
+      firstName:"Hasan",
+      lastName:"Ali",
+      Salary: 10000,
+    };
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={style}>hello my dear {name}</p>
+
+        <h3 style={{color:'yellow'}}>I am Mahmudul, I realy love you, I don't know what do you think about me but I know you love me but some confusion have in your mind. dear {name} don't worry I'm only for you.</h3>
+        <h2>your name is {person1.firstName +" "+ person1.lastName}</h2>
+        <p>Your Salary is: {person1.Salary+bonous}</p>
       </header>
     </div>
   );
