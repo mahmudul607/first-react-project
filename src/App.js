5315aabfff479d8c963828c03a358eab08e8a734
mahmudul607 <111509406+mahmudul607@users.noreply.github.com>
2023-01-07 19:11:39 +0600

    component Person is added

--- src/App.js
+++ src/App.js
@@ -23,9 +23,27 @@ function App() {
         <h3 style={{color:'yellow'}}>I am Mahmudul, I realy love you, I don't know what do you think about me but I know you love me but some confusion have in your mind. dear {name} don't worry I'm only for you.</h3>
         <h2>your name is {person1.firstName +" "+ person1.lastName}</h2>
         <p>Your Salary is: {person1.Salary+bonous}</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
+    
   );
+}
+
+function Person(){
+  const personStyle = {
+    border:'2px solid red',
+    margin:'10px',
+    padding:'10px'
+  }
+  return ( 
+    <div style={personStyle}>
+      <h1 >Start Work with React</h1>
+      <h3>2nd day of react</h3>
+    </div>
+  )
 }
 
 export default App;
